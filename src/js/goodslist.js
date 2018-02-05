59c6de10aec3a0dcfc72bd3f186affc35b888c79
Marco-hui <matao_1891@163.com>
2018-02-05 18:17:08 +0800

    完成列表页筛选、排序、浏览记录部分

--- src/js/goodslist.js
+++ src/js/goodslist.js
@@ -46,5 +46,46 @@ require(['config'],function(){
             })
         }
         likes();
+
+        // 收货地 三级联动
+        $.get("../api/data/region.json",function(res){
+            var data=res.regions;
+
+            var province=$('#sort_province')[0];
+            var city=$('#sort_city')[0];
+            var county=$('#sort_county')[0];
+            // 生成省列表
+            for(var i=0;i<data.length;i++){
+                var option_province=document.createElement('option');
+                option_province.value=data[i].name;
+                option_province.innerText=data[i].name;
+                province.appendChild(option_province);
+            }
+
+            // 根据选择的省生成相应的城市列表
+            province.onclick=function(e){
+                for(var i=0;i<data.length;i++){
+                    if(data[i].name == e.target.value){
+                        var data_city=data[i].regions;
+                        var option_city=data_city.map(function(item){
+                            return `<option value="${item.name}">${item.name}</option>`
+                        }).join('');
+                        // 根据选择的城市生成相应的县级列表
+                        city.onclick=function(evt){
+                            for(var j=0;j<data_city.length;j++){
+                                if(data_city[j].name == evt.target.value){
+                                    var data_county=data_city[j].regions;
+                                    var option_count=data_county.map(function(items){
+                                        return `<option value="${items.name}">${items.name}</option>`
+                                    }).join('');
+                                }
+                            }
+                            county.innerHTML="<option value='请选择'>请选择</option>"+option_count;
+                        }
+                    }
+                }
+                city.innerHTML="<option value='请选择'>请选择</option>"+option_city;
+            }
+        },'json')
     })
 })
